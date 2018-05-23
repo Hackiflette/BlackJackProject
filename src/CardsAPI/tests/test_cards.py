8e8739096073cb050377696370fcb9e955292e27
Hugo <hugoboniface@gmail.com>
2018-05-23 21:48:52 +0200

    Add card tests

---
+++ src/CardsAPI/tests/test_cards.py
@@ -0,0 +1,21 @@
+import Card
+
+# To launch test in console :
+# ... $ python -m pytest tests/
+
+def test_create_object():
+
+    card_1 = Card.Card(1)
+    value = card_1.value
+    assert value == 1
+
+def test_create_object_str():
+
+    card = Card.Card('TWO')
+    assert card.value == 2
+
+def test_create_object_value_entire():
+
+    card = Card.Card('Three', 'diamond')
+    assert card.value == 3
+    assert card.color == 'DIAMONDS'
