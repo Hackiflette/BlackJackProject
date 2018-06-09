7c49bea3e5e16b848e2c7d85fa100de757e35b83
deplanty <yvon.deplante@gmail.com>
2018-06-09 20:27:57 +0200

    modify src/common

--- src/common/constants.py
+++ src/common/constants.py
@@ -7,11 +7,17 @@ File containing all the constant variables
 import os
 import json
 
-__config = "data/config/path.cfg.json"
-with open(__config, "r", encoding="utf-8") as f:
-    cfg = json.load(f)
+__tmp = "data/config/path.cfg.json"
+with open(__tmp, "r", encoding="utf-8") as f:
+    CONFIG_PATH = json.load(f)
+__tmp = "data/config/pictures.cfg.json"
+with open(__tmp, "r", encoding="utf-8") as f:
+    CONFIG_PICTURES = json.load(f)
+__tmp = "data/config/ui.cfg.json"
+with open(__tmp, "r", encoding="utf-8") as f:
+    CONFIG_UI = json.load(f)
 
-DIR_SRC = os.path.join(*cfg["folders"]["src"])  # Source files
-DIR_DATA = os.path.join(*cfg["folders"]["data"])  # Data files
-DIR_CONFIG = os.path.join(*cfg["folders"]["config"])  # Configuration files
-DIR_PICTURES = os.path.join(*cfg["folders"]["pictures"])  # Pictures
+DIR_SRC = os.path.join(*CONFIG_PATH["folders"]["src"])  # Source files
+DIR_DATA = os.path.join(*CONFIG_PATH["folders"]["data"])  # Data files
+DIR_CONFIG = os.path.join(*CONFIG_PATH["folders"]["config"])  # Configuration files
+DIR_PICTURES = os.path.join(*CONFIG_PATH["folders"]["pictures"])  # Pictures
---
+++ src/common/func_pictures.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from src.common.constants import *
+
+
+def load_image(file):
+    """
+    Loads an image, prepares it for play
+    """
+
+    file = os.path.join(DIR_PICTURES, file)
+    try:
+        surface = pygame.image.load(file)
+    except pygame.error:
+        error = "Could not load image \"%s\" %s" % (file, pygame.get_error())
+        raise SystemExit(error)
+    return surface.convert()
