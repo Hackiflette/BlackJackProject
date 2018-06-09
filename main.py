2f65342d8c32fbd8b9b0701732577f4408280b9c
Hugo <hugoboniface@gmail.com>
2018-06-09 21:08:31 +0200

    Create main of BlackJack to view menu

--- main.py
+++ main.py
@@ -8,7 +8,11 @@
 
 import os
 
+import pygame
+from pygame.locals import *
+
 from src.common.constants import *
+from src import view_menu
 
 # ============================================================================
 # =
@@ -16,6 +20,55 @@ from src.common.constants import *
 # =
 # ============================================================================
 
-print("Exists?")
-for folder in [DIR_SRC, DIR_DATA, DIR_CONFIG, DIR_PICTURES]:
-    print(folder, os.path.exists(folder))
+
+class Main:
+    """
+    The main application
+    """
+
+    def __init__(self):
+
+        # Configuration
+        self.loadConfig()
+
+        # Init pygame
+        self.initPygame()
+
+        # mainloop
+        self.mainloop()
+
+    def loadConfig(self):
+        """
+        Load the configuration file
+        """
+
+        file_config = os.path.join(DIR_CONFIG, "ui.cfg.json")
+        with open(file_config, "r", encoding="utf-8") as f:
+            self.config = json.load(f)
+
+    def initPygame(self):
+        """
+        Intialize the main screen
+        """
+
+        pygame.init()
+        pygame.display.set_caption("Black Jack by Hackiflette")
+        self.window = pygame.display.set_mode((self.config["window"]["width"],
+                                               self.config["window"]["height"]))
+
+    def mainloop(self):
+        """
+        The mainloop the launch the game
+        """
+
+        state, param = view_menu.main(self.window, self.config["window"])
+
+        pygame.quit()
+
+
+if __name__ == '__main__':
+    print("Exists?")
+    for folder in [DIR_SRC, DIR_DATA, DIR_CONFIG, DIR_PICTURES]:
+        print(folder, os.path.exists(folder))
+
+    Main()
