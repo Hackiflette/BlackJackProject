9f7985249f34a0c429135753f0a02ffb5e861333
unknown <LAxelle>
2018-07-14 17:56:17 +0200

    Test for deck

---
+++ src/CardsAPI/tests/test_deck.py
@@ -0,0 +1,54 @@
+from src.CardsAPI import Deck,Card
+
+def testInitDeck():
+
+    deck = Deck.Deck()
+    assert deck.top_card_index == 0
+    assert len(deck.cards) == 312
+    assert deck.red_card_index >= ((3 / 4) * 312) - 30 and deck.red_card_index <= ((3 / 4) * 312) + 30
+
+    # Assert not shuffle
+    deck = Deck.Deck(False)
+    assert deck.top_card_index == 0
+    assert len(deck.cards) == 312
+    assert deck.red_card_index >= ((3 / 4) * 312) - 30 and deck.red_card_index <= ((3 / 4) * 312) + 30
+
+def testDealDeck():
+
+    deck = Deck.Deck()
+    card = deck.deal()
+    assert type(card) == Card.Card
+    assert deck.top_card_index == 1
+
+def testRedCardDeck():
+
+    deck = Deck.Deck()
+    last_card = deck.cards[-1]
+    deck.deal()
+    assert last_card == deck.cards[-1]
+
+    for i in range(int(((3 / 4) * 312) + 35)):
+        deck.deal()
+
+    assert deck.cards[-1] != last_card
+
+def testShuffleDeck():
+
+    deck = Deck.Deck()
+    first_card = deck.cards[0]
+    deck.shuffle()
+    assert first_card != deck.cards[0]
+
+def testInitShuffleDeck():
+
+    deck1 = Deck.Deck(False)
+    deck2 = Deck.Deck(False)
+    assert deck1.cards[0] == deck2.cards[0]
+
+def testToMuchDeal():
+
+    deck = Deck.Deck()
+    for i in range(500):
+        card = deck.deal()
+        
+    assert len(deck.cards) == 312
