643f67c35037cd8ae9a0b351580ad1941275be88
Geoffrey Erhard <geoerh@gmail.com>
2018-07-14 16:29:53 +0200

    option button + path to view_option

---
+++ src/views/view_option.py
@@ -0,0 +1,52 @@
+import pygame
+from pygame.locals import *
+
+from src.common.constants import *
+from src.common.func_pictures import load_image
+
+def main(window, menu_config):
+    """ The main function of the view of options"""
+
+
+  # Init window
+    screen = window
+
+    # Load background image
+    bgd_tile = load_image("menu/bgd_menu.png")
+    background = pygame.Surface((menu_config["width"], menu_config["height"]))
+    background.blit(bgd_tile, (0, 0))
+
+    # Prepare text
+    title_font = pygame.font.Font(None, 44)
+    title_text = title_font.render("Options", 2, (255, 255, 255))
+
+    # Display on windows
+    screen.blit(background, (0, 0))
+    screen.blit(title_text, (80, 30))
+    pygame.display.flip()
+
+    # Init sprites
+    all_sprites = pygame.sprite.RenderUpdates()
+    clock = pygame.time.Clock()
+
+    state = Game.option
+    while state == Game.option:
+
+        # Clear all the sprites
+        all_sprites.clear(screen, bgd_tile)
+        all_sprites.update()
+
+        # Check for events
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                state = Game.menu
+            elif (event.type == KEYDOWN and event.key == K_ESCAPE):
+                state = Game.menu
+
+        # Update the scene
+        dirty = all_sprites.draw(screen)
+        pygame.display.update(dirty)
+
+        clock.tick(40)
+
+    return state, dict()
