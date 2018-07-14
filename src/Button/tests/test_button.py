0299cc77c8fbf87aaed91dd2861195e2986fabcc
Hugo <hugoboniface@gmail.com>
2018-07-14 22:03:56 +0200

    Create Button tests

---
+++ src/Button/tests/test_button.py
@@ -0,0 +1,12 @@
+from src.Button import Button
+
+def testInitButton():
+    try:
+        button = Button(text="Test")
+        assert True
+    except:
+        assert False
+
+def testButtonPos():
+    btn = Button(text="Test")
+    assert btn.rect.x == btn.rect.y == 0
