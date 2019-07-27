99d1d3ca68f04a7d018fe0bef626e026c67f5aad
deplanty <yvon.deplante@gmail.com>
2019-07-27 21:40:11 +0200

    Update test to use abspath

--- src/common/tests/test_config.py
+++ src/common/tests/test_config.py
@@ -9,5 +9,5 @@ def test_config_path():
     with open(file_config_path) as f:
         paths = json.load(f)
     
-    assert ConfigPath.get("folders", "data") == os.path.join(*paths["folders"]["data"])
-    assert ConfigPath.folder("data") == os.path.join(*paths["folders"]["data"])
+    assert ConfigPath.get("folders", "data") == os.path.abspath(os.path.join(*paths["folders"]["data"]))
+    assert ConfigPath.folder("data") == os.path.abspath(os.path.join(*paths["folders"]["data"]))
