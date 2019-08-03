451649c7edfc08aa06a6bf4a89367f1c527fbbdd
Hboni <hugoboniface@gmail.com>
2019-08-03 12:44:34 +0200

    update tests for new module names

--- src/cards/tests/test_cards.py
+++ src/cards/tests/test_cards.py
@@ -1,4 +1,4 @@
-from src.cards import card
+from src.cards.card import Card
 
 # To launch test in console :
 # ... $ python -m pytest tests/
@@ -6,44 +6,44 @@ from src.cards import card
 
 def test_create_num_card():
 
-    card = card.Card(1)
+    card = Card(1)
     assert card.value == 1
     assert card.color == 'HEARTS'
 
-    card = card.Card('TWO')
+    card = Card('TWO')
     assert card.value == 2
 
-    card = card.Card('Three', 'diamond')
+    card = Card('Three', 'diamond')
     assert card.value == 3
     assert card.color == 'DIAMONDS'
 
 
 def test_create_face_card():
 
-    card = card.Card(12)
+    card = Card(12)
     assert card.name == "QUEEN"
     assert card.value == 10
 
-    card = card.Card("King")
+    card = Card("King")
     assert card.name == "KING"
     assert card.color == 'HEARTS'
 
 
 def test_add_cards():
 
-    card_1 = card.Card(5, "Hearts")
-    card_2 = card.Card(7, "DIAMONDS")
+    card_1 = Card(5, "Hearts")
+    card_2 = Card(7, "DIAMONDS")
     assert card_1 + card_2 == 12
 
-    card_3 = card.Card("TWO", "spades")
-    card_4 = card.Card(13)
+    card_3 = Card("TWO", "spades")
+    card_4 = Card(13)
     assert card_3 + card_4 == 12
 
 
 def test_add_ace():
 
-    card_1 = card.Card("KING", "hearts")
-    card_2 = card.Card(9)
-    card_3 = card.Card("ACE", "heaRts")
+    card_1 = Card("KING", "hearts")
+    card_2 = Card(9)
+    card_3 = Card("ACE", "heaRts")
     assert card_1 + card_2 + card_3 == 20
     # assert card_3 + card_2 + card_1 == 20
--- src/cards/tests/test_hand.py
+++ src/cards/tests/test_hand.py
@@ -1,4 +1,5 @@
-from src.cards import card, hand
+from src.cards.hand import Hand
+from src.cards.card import Card
 
 # To launch test in console :
 # ... $ python -m pytest tests/
@@ -7,19 +8,19 @@ from src.cards.exceptions import CardsAPIError
 
 def test_init_hand():
 
-    hand = hand.Hand()
+    hand = Hand()
     assert len(hand.card_list) == 0
 
-    hand = hand.Hand([card.Card(2)])
+    hand = Hand([Card(2)])
     assert hand.value == 2
 
 
 def test_add_card():
 
-    hand = hand.Hand()
-    card = card.Card(2)
+    hand = Hand()
+    card = Card(2)
     hand = hand + card
-    card = card.Card(5)
+    card = Card(5)
     hand = hand + card
     assert len(hand.card_list) == 2
     assert hand.value == 7
@@ -27,30 +28,30 @@ def test_add_card():
 
 def test_iadd_card():
 
-    hand = hand.Hand([card.Card(5)])
+    hand = Hand([Card(5)])
     hand_list = [hand]
-    hand += card.Card(2)
+    hand += Card(2)
     assert len(hand_list[0].card_list) == 2
     assert hand_list[0].value == 7
 
 
 def test_comparison_hand():
 
-    hand_1 = hand.Hand([card.Card(5),
-                        card.Card(7)])
-    hand_2 = hand.Hand([card.Card(3)])
+    hand_1 = Hand([Card(5),
+                   Card(7)])
+    hand_2 = Hand([Card(3)])
     assert hand_1 > hand_2
 
 
 def test_split_hand():
 
-    hand = hand.Hand([card.Card(3, 'heart'),
-                      card.Card(3, 'spades')])
+    hand = Hand([Card(3, 'heart'),
+                 Card(3, 'spades')])
     [hand_1, hand_2] = hand.split()
     assert hand_1.value == 3 and hand_2.value == 3
     assert hand_1 == hand_2
 
-    hand2 = hand.Hand([card.Card(3), card.Card(4)])
+    hand2 = Hand([Card(3), Card(4)])
     try:
         hand2.split()
     except CardsAPIError:
@@ -59,7 +60,7 @@ def test_split_hand():
         assert False
 
     # testing too many cards in hand
-    hand += card.Card(3)
+    hand += Card(3)
     try:
         hand.split()
     except CardsAPIError:
